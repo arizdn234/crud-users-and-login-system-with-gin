c70128160286a27d8b884166b509b7a3814b919b
Arizidane <samionohari@gmail.com>
2024-05-10 12:52:31 +0700

    add: CreateToken function & VerifyToken function

---
+++ utils/jwt.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtKey = []byte("secret_key")
+
+type Claims struct {
+	ID    uint   `json:"id"`
+	Email string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func CreateToken(id uint, email string) (string, error) {
+	claims := &Claims{
+		ID:    id,
+		Email: email,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 1)), // 1 hour
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func VerifyToken(tokenString string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return claims, nil
+}
