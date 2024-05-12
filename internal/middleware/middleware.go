4f397ab0412901bcfcabff4fc4f7a509cb32974b
Arizidane <samionohari@gmail.com>
2024-05-13 06:21:44 +0700

    fix: RequireAuth with VerifyToken features

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,16 +3,25 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/arizdn234/crud-users-and-login-system-with-gin/utils"
 	"github.com/gin-gonic/gin"
 )
 
 func RequireAuth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		cookie, err := c.Cookie("auth_token")
-		if err != nil || cookie == "" {
+		authToken, err := c.Cookie("auth_token")
+		if err != nil || authToken == "" {
 			c.AbortWithStatus(http.StatusUnauthorized)
 			return
 		}
+
+		tokenClaims, err := utils.VerifyToken(authToken)
+		if err != nil {
+			c.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		c.Set("id", tokenClaims.ID)
 
 		c.Next()
 	}
