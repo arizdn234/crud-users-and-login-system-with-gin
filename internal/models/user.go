4b5276849576504807f064567203337863ac170a
Arizidane <samionohari@gmail.com>
2024-05-13 03:46:18 +0700

    fix: user model struct

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,10 +3,21 @@ package models
 import "time"
 
 type User struct {
-	ID        uint      `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name      string    `json:"name" gorm:"type:varchar(255);"`
+	Email     string    `json:"email" gorm:"type:varchar(255);not null"`
+	Password  string    `json:"password" gorm:"type:varchar(255);not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type UserLogin struct {
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
+
+type UserRegister struct {
+	Name     string `json:"name" binding:"required"`
+	Email    string `json:"email" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
